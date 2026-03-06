9bbfe2b75f43b9632eba04f5473929516f1b29c9
ThiagoM8 <thiagomendesc998@gmail.com>
2026-03-06 13:07:30 -0300

    FIX: NAME ISSUES IN VERSION GEMINI

--- brain.py
+++ brain.py
@@ -34,7 +34,7 @@ def processar_e_salvar(whatsapp_id, texto_usuario):
     try:
         # Chama o modelo 2.0 Flash para processamento ultra rápido
         response = client.models.generate_content(
-            model="gemini-1.5-flash",
+            model="models/gemini-1.5-flash",
             contents=prompt
         )
         
